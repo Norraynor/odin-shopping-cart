3c456e6fe5b319948c0f0f5772aceeb497807646
norraynor <37270508+Norraynor@users.noreply.github.com>
2025-03-02 19:34:17 +0100

    Setup Home page

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,1 +1,35 @@
 //homepage goes here
+import pepeIcon from "./assets/pepe-icon.webp";
+function Home() {
+	return (
+		<>
+			<div className="container">
+				<div className="nav-bar">
+					{/*LOGO and NAME || Home Shop About */}
+					<div className="logo">
+						<div className="icon">
+							<img src="./assets/pepe-icon.webp" alt="pepe icon" />
+						</div>
+						<div className="name">Pepe Market</div>
+					</div>
+					<div className="nav">
+						{/* links to pages --router stuff? */}
+						<div className="home"></div>
+						<div className="shop"></div>
+						<div className="about"></div>
+					</div>
+				</div>
+				<div className="main">
+					{/* some photo maybe or text? */}
+					<h1 className="welcome">Welcome to Pepe Market</h1>
+					<h2 className="sub-welcome">The best place to get your own Pepe</h2>
+				</div>
+				<div className="footer">
+					{/* link to github? */}
+					<h5 className="creator">Made by Norraynor</h5>
+				</div>
+			</div>
+		</>
+	);
+}
+export default Home;
--- src/Shop.jsx
+++ src/Shop.jsx
@@ -1,1 +1,13 @@
 //shop page goes here with possibility to add items to cart
+function Shop() {
+	return (
+		<>
+			<div className="container">
+				<h1>Our Shop</h1>
+				{/* add items here */}
+			</div>
+		</>
+	);
+}
+
+export default Shop;
