d970cff0268e2c583550f0823e172c7e9f44f1ac
norraynor <37270508+Norraynor@users.noreply.github.com>
2025-03-17 20:31:54 +0100

    Add style to card

--- src/Card.jsx
+++ src/Card.jsx
@@ -4,7 +4,9 @@ function Card(props) {
 	return (
 		<>
 			<div className="item">
-				<img className="item-image" src={props.card.image} alt="" />
+				<div className="item-image">
+					<img src={props.card.image} alt="" />
+				</div>
 				<h4 className="title">{props.card.title}</h4>
 				<h5 className="price">Price: {props.card.price}</h5>
 			</div>
